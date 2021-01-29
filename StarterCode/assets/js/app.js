028ca64074bc1d4ba70145a636494c6c766ba55a
RUBALBHULLAR <69098507+RUBALBHULLAR@users.noreply.github.com>
2021-01-29 14:17:55 -0700

    Update app.js

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,8 +1,48 @@
-// @TODO: YOUR CODE HERE!
-d3.csv("/assets/data/data.csv").then(function(stateData) {
-    // Format the data
-        stateData.forEach(function(data) {
+// D3 Animated Scatter Plot
+// we need to at least set up the width, height and margins of the graph.
+// Grab the width of the containing box
+var width = parseInt(d3.select("#scatter").style("width"));
 
-            data.poverty = +data.poverty;
-            data.healthcare = +data.healthcare;
-        });
+// Designate the height of the graph
+var height = width - width / 3.9;
+
+// Margin spacing for graph
+var margin = 20;
+
+// space for placing words
+var labelArea = 110;
+
+// padding for the text at the bottom and left axes
+var tPadBot = 40;
+var tPadLeft = 40;
+
+// Create the actual canvas for the graph
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("class", "chart");
+
+// Set the radius for each dot that will appear in the graph.
+// Note: Making this a function allows us to easily call
+// it in the mobility section of our code.
+var circRadius;
+function crGet() {
+  if (width <= 530) {
+    circRadius = 5;
+  }
+  else {
+    circRadius = 10;
+  }
+}
+crGet();
+
+// The Labels for our Axes
+// A) Bottom Axis
+// ==============
+
+// We create a group element to nest our bottom axes labels.
+svg.append("g").attr("class", "xText");
+// xText will allows us to select the group without excess code.
+var xText = d3.select(".xText");
