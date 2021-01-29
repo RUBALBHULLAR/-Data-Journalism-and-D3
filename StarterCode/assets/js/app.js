6b89e318b0d3f9bff294d3e45d5aba89fe807018
RUBALBHULLAR <69098507+RUBALBHULLAR@users.noreply.github.com>
2021-01-29 02:20:14 -0700

    Update app.js

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,8 @@
 // @TODO: YOUR CODE HERE!
+d3.csv("/assets/data/data.csv").then(function(stateData) {
+    // Format the data
+        stateData.forEach(function(data) {
+
+            data.poverty = +data.poverty;
+            data.healthcare = +data.healthcare;
+        });
